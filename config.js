9ac5e24b9a888cc8fb3a9c06deb0360bac400882
Ivan Pykalo <ipyka@softserveinc.com>
2021-07-10 20:46:48 +0300

    deleted images folder

--- config.js
+++ config.js
@@ -18,70 +18,60 @@ const PAGES = {
         view: '',
         pageTitle: 'Shop',
         menuItem: 'Shop',
-        image: '',
         route: ROUTES.INDEX
     },
     productList: {
         view: 'shop/product-list',
         pageTitle: 'Shop',
         menuItem: 'All Products',
-        image: '/images/book.jpg',
         route: ROUTES.PRODUCTS
     },
     productDetail: {
         view: 'shop/product-detail',
         pageTitle: 'Shop',
         menuItem: 'Product Detail',
-        image: '',
         route: ROUTES.PRODUCT_DETAIL_ID
     },
     checkout: {
         view: 'shop/checkout',
         pageTitle: 'Checkout',
         menuItem: 'Checkout',
-        image: '',
         route: ROUTES.CHECKOUT
     },
     cart: {
         view: 'shop/cart',
         pageTitle: 'Your Cart',
         menuItem: 'Cart',
-        image: '',
         route: ROUTES.CART
     },
     index: {
         view: 'shop/index',
         pageTitle: 'Shop',
         menuItem: 'Index',
-        image: '',
         route: ROUTES.INDEX
     },
     orders: {
         view: 'shop/orders',
         pageTitle: 'Shop',
         menuItem: 'Orders',
-        image: '',
         route: ROUTES.ORDERS
     },
     products: {
         view: 'admin/products',
         pageTitle: 'Admin Products',
         menuItem: 'Admin Products',
-        image: '',
         route: ROUTES.ADMIN_PRODUCTS
     },
     addProduct: {
         view: 'admin/add-product',
         pageTitle: 'Add Product',
         menuItem: 'Add Product',
-        image: '',
         route: ROUTES.ADMIN_ADD_PRODUCT
     },
     editProduct: {
         view: 'admin/edit-product',
         pageTitle: 'Edit Product',
         menuItem: 'Edit Product',
-        image: '',
         route: ROUTES.ADMIN_EDIT_PRODUCT
     },
     notFound: {
