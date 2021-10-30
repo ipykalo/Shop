d68db4f9fcb2725ea74eeb5cf88088de30af56b4
Ivan Pykalo <ipyka@softserveinc.com>
2021-10-30 17:46:26 +0300

    fixed

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const config = require('./config');
 
 mongoose.connect(config.MONGO_DB_DRIVER)
     .then(() => {
-        //app.listen(process.env.PORT || 3000, () => console.log("Server is runing!"))
-        https.createServer({
-            key: fs.readFileSync(path.join(__dirname, 'ssl', 'key.pem')),
-            cert: fs.readFileSync(path.join(__dirname, 'ssl', 'cert.pem'))
-        }, app)
-            .listen(process.env.PORT || 3000, () => console.log(`Server is runing on the port: ${process.env.PORT}`));
+        app.listen(process.env.PORT || 3000, () => console.log("Server is runing!"))
+        // https.createServer({
+        //     key: fs.readFileSync(path.join(__dirname, 'ssl', 'key.pem')),
+        //     cert: fs.readFileSync(path.join(__dirname, 'ssl', 'cert.pem'))
+        // }, app)
+        //     .listen(process.env.PORT || 3000, () => console.log(`Server is runing on the port: ${process.env.PORT}`));
     })
     .catch(err => console.log(err, 'db connection'));
